489853a01a593b50f0a7b32a7c8599320d0a7db1
gtongy <32930281+gtongy@users.noreply.github.com>
2020-09-20 17:50:22 +0900

    :sparkles: modify output file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use envy;
 use serde_derive::Deserialize;
+use std::fs::File;
+use std::io::Error;
 use std::process;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 #[derive(Deserialize, Debug)]
 struct Config {
@@ -12,7 +14,7 @@ struct Config {
     schema: String,
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     let config = match envy::from_env::<Config>() {
         Ok(val) => val,
         Err(err) => {
@@ -21,7 +23,8 @@ fn main() {
         }
     };
     const PARALLEL_NUM: u16 = 3;
-    const FILENAME: &str = "demo.sql";
+    let outputs = File::create("output.sql")?;
+    let errors = outputs.try_clone()?;
     Command::new("mysqlpump")
         .arg(format!("{}{}", "-u", config.user_name))
         .arg(format!("{}{}", "-p", config.password))
@@ -30,7 +33,9 @@ fn main() {
         .args(&["--single-transaction", "--skip-column-statistics"])
         .arg(format!("{}={}", "--default-parallelism", PARALLEL_NUM))
         .arg(format!("{}", config.schema))
-        .arg(format!("> {}", FILENAME))
+        .stdout(Stdio::from(outputs))
+        .stderr(Stdio::from(errors))
         .status()
         .expect("failed to execute process");
+    Ok(())
 }
