6bd87fee7c89345780dc3193987df1c5d56a8cfb
gtongy <32930281+gtongy@users.noreply.github.com>
2020-09-21 20:25:18 +0900

    :sparkles: modify slack notify

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use futures::{FutureExt, TryStreamExt};
 use rusoto_core::Region;
 use rusoto_s3::*;
 use serde_derive::Deserialize;
+use slack_hook::SlackTextContent::Text;
+use slack_hook::{PayloadBuilder, Slack};
 use std::fs::File;
 use std::io::Error;
 use std::process;
@@ -17,6 +19,8 @@ struct Config {
     port: u16,
     schema: String,
     bucket_name: String,
+    slack_hook: String,
+    slack_channel_name: String,
 }
 
 #[tokio::main]
@@ -58,7 +62,7 @@ async fn main() -> Result<(), Error> {
             .into_stream()
             .map_ok(Bytes::from);
         let req = PutObjectRequest {
-            bucket: String::from(config.bucket_name),
+            bucket: config.bucket_name.clone(),
             key: String::from(OUTPUT_FILE_NAME),
             content_length: Some(meta.len() as i64),
             body: Some(StreamingBody::new(read_stream)),
@@ -69,6 +73,22 @@ async fn main() -> Result<(), Error> {
             .put_object(req)
             .await
             .expect("Couldn't PUT object");
+        let slack = Slack::new(config.slack_hook.as_ref()).unwrap();
+        let url = format!(
+            "https://{}.s3-ap-northeast-1.amazonaws.com/{}",
+            &config.bucket_name, OUTPUT_FILE_NAME
+        );
+        let p = PayloadBuilder::new()
+            .text(vec![Text("file download link get!\n".into()), Text(url.into())].as_slice())
+            .channel(config.slack_channel_name)
+            .build()
+            .unwrap();
+
+        let res = slack.send(&p);
+        match res {
+            Ok(()) => println!("ok"),
+            Err(x) => println!("ERR: {:?}", x),
+        }
     }
     Ok(())
 }
